aee2446eb45caea52d91b58ff69fcf0948514ec8
Juliet Shin <juliet.shin@ucop.edu>
2026-08-25 10:01:44 -0700

    Added missing favicon on landing page

--- app/(embed)/[locale]/layout.tsx
+++ app/(embed)/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
+import type { Metadata } from 'next';
 import { ApolloWrapper } from "@/lib/graphql/apollo-wrapper";
 import { CsrfProvider } from "@/context/CsrfContext";
 
@@ -18,6 +19,27 @@ const font_sans_serif = Poppins({
   variable: '--font-sans-serif',
   preload: false, // ← stops Next.js injecting preload hints, since preloading is adding warnings in browser, and font should be cached anyways after first page
 });
+
+export const metadata: Metadata = {
+  icons: {
+    icon: [
+      {
+        url: `https://${process.env.CDN_ENDPOINT}/assets/dmptool-logo-light.svg`,
+        type: 'image/svg+xml',// Default icon for browsers that don't support media queries
+      },
+      {
+        url: `https://${process.env.CDN_ENDPOINT}/assets/dmptool-logo-light.svg`,
+        type: 'image/svg+xml',
+        media: '(prefers-color-scheme: light)',
+      },
+      {
+        url: `https://${process.env.CDN_ENDPOINT}/assets/dmptool-logo-dark.svg`,
+        type: 'image/svg+xml',
+        media: '(prefers-color-scheme: dark)',
+      },
+    ],
+  },
+};
 
 export default async function EmbedLayout({
   children,
